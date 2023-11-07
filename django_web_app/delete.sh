dba5f6807a93792d775d0c8ede4776a2c75b392b
Batchen <b0504144053@gmail.com>
2023-11-07 14:13:07 +0200

    start

---
+++ django_web_app/delete.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+version=$1
+if [ "" =  "$version" ]; then
+    echo "Error: Missing parameter, Run again with the version did you want."
+    exit 1
+fi
+
+docker stop dev_connect_app_con
+docker rm dev_connect_app_con
+docker rmi dev_connect_app:${version}
+# docker volume rm data-vol
