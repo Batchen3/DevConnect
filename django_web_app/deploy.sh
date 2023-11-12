debf06773ca7d0a409d0aad9a3176dda8834663a
Batchen <b0504144053@gmail.com>
2023-11-12 11:38:34 +0200

    change

--- django_web_app/deploy.sh
+++ django_web_app/deploy.sh
@@ -1,5 +1,5 @@
 img_name_and_tag=$1
 docker build -t ${img_name_and_tag} .
-gcloud auth configure-docker us-central1-docker.pkg.dev
-docker tag ${img_name_and_tag} us-central1-docker.pkg.dev/devconnect-final-project/batchen-artifacts/${img_name_and_tag}
-docker push us-central1-docker.pkg.dev/devconnect-final-project/batchen-artifacts/${img_name_and_tag}
+gcloud auth configure-docker me-west1-docker.pkg.dev
+docker tag ${img_name_and_tag} me-west1-docker.pkg.dev/devconnect-project/batchen-artifacts/${img_name_and_tag}
+docker push me-west1-docker.pkg.dev/devconnect-project/batchen-artifacts/${img_name_and_tag}
